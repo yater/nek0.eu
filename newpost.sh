b531606ad16750f7aaee3db5e0aeec1153be58a0
nek0 <nek0@chelnok.de>
2016-03-04 16:42:36 +0100

    changed script to bash

---
+++ newpost.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+date=$(date --rfc-3339=date)
+title=
+author=
+
+function input {
+	echo "Enter post title:"
+	read title
+	echo "Enter author name:"
+	read author
+}
+
+function confirmation {
+	echo "Is the following correct?"
+	echo "title: $title"
+	echo "author: $author"
+	echo "y/n"
+}
+
+function main {
+	local conf="n"
+	while [ "$conf" != "y" ]
+	do
+		input
+		confirmation
+		read conf
+	done
+	set -x
+	postname="./posts/$date-${title// /-}.md"
+	echo -e "---\ntitle: ${title}\nauthor: ${author}\ntags: \ndescription: \n---" >> $postname
+	vim $postname
+}
+
+main
+
+unset date title author
