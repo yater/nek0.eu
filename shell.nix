254895f46138f83ec638e33805c1257e8d0a0a56
nek0 <nek0@chelnok.de>
2018-03-30 23:26:13 +0200

    nixify

---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, directory, filepath, hakyll, HTTP
+      , stdenv, time
+      }:
+      mkDerivation {
+        pname = "nek0-blog";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base directory filepath hakyll HTTP time
+        ];
+        homepage = "https://nek0.eu";
+        description = "My blog";
+        license = stdenv.lib.licenses.unfree;
+        hydraPlatforms = stdenv.lib.platforms.none;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
